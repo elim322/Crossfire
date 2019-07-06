a2eeffce79b80cd1fddf3a63953f4765ce061283
elim322 <einer.lim322@gmail.com>
2019-07-06 16:13:46 -0700

    add dummy cast data

--- src/components/cast.js
+++ src/components/cast.js
@@ -1,9 +1,113 @@
 import React from "react"
 
 const Cast = () => (
-  <>
-    <h1>Cast</h1>
-  </>
+  <React.Fragment>
+    <div className="cast-title">
+      <h1> Our Amazing Cast</h1>
+    </div>
+    <div className="cast-list">
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+      <div className="member">
+        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <p className="cast-name">tim</p>
+        <p className="cast-role">UX designer</p>
+      </div>
+    </div>
+  </React.Fragment>
 )
 
 export default Cast
