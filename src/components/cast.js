ab9feb3df42ae465928940235353d8f736143793
elim322 <einer.lim322@gmail.com>
2019-07-23 21:51:28 -0700

    move images dir

--- src/components/cast.js
+++ src/components/cast.js
@@ -9,102 +9,102 @@ const Cast = () => (
     </div>
     <div className="cast-list">
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
       <div className="member">
-        <img className="cast-image" src={require("../assets/images/tim.jpg")} />
+        <img className="cast-image" src={require("../images/tim.jpg")} />
         <p className="cast-name">tim</p>
         <p className="cast-role">UX designer</p>
       </div>
