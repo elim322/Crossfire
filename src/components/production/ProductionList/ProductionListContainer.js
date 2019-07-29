291425acdfb9e1295ea4fddbc7c03e6f8caa8a9b
aj <ajaguasin@gmail.com>
2019-07-28 21:34:47 -0700

    Create modal component, organize sass files

--- src/components/production/ProductionList/ProductionListContainer.js
+++ src/components/production/ProductionList/ProductionListContainer.js
@@ -2,7 +2,28 @@ import React, { Component } from "react"
 import ProductionListItem from "./ProductionListItem"
 
 export default class ProductionListContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      isModalOpen: false,
+    }
+  }
+  toggleModal = () => {
+    document.body.classList.contains("modalOpen")
+      ? document.body.classList.remove("modalOpen")
+      : document.body.classList.add("modalopen")
+
+    this.setState(state => {
+      return { isModalOpen: !state.isModalOpen }
+    })
+  }
+
   render() {
-    return <ProductionListItem />
+    return (
+      <ProductionListItem
+        toggleModal={this.toggleModal}
+        isModalOpen={this.state.isModalOpen}
+      />
+    )
   }
 }
--- src/components/ui/ModalComponent.js
+++ src/components/ui/ModalComponent.js
@@ -1,7 +1,28 @@
-import React, { Component } from "react"
+import React from "react"
+import tim from "../../assets/images/tim.jpg"
 
-export default class ModalComponent extends Component {
-  render() {
-    return <div>Modal</div>
-  }
+const ModalComponent = ({ children }, imgSrc, toggleModal) => {
+  return (
+    <div className="modal">
+      <div className="modalBody">
+        <div className="modalImage">
+          <img src={imgSrc === !undefined ? imgSrc : tim} />
+        </div>
+        <div className="modalContent">
+          <h1>Title</h1>
+          <h2>Subtitle</h2>
+          {children}
+        </div>
+        <div
+          className="closeButton"
+          onClick={() => {
+            toggleModal()
+          }}
+        >
+          <span>close</span>
+        </div>
+      </div>
+    </div>
+  )
 }
+export default ModalComponent
