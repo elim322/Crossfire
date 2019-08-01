00e5c9701546f82dfea65548b2e8a68a09dec27f
aj <ajaguasin@gmail.com>
2019-07-31 20:08:01 -0700

    Make modal reusable, add photos for blog

--- src/components/ui/ModalComponent.js
+++ src/components/ui/ModalComponent.js
@@ -1,20 +1,10 @@
 import React from "react"
-import tim from "../../assets/images/tim.jpg"
 import closeIcon from "../../assets/images/crossIcon.svg"
 const ModalComponent = props => {
   return (
     <div className="modal">
       <div className="modalBody">
-        <div className="modalImage">
-          {/* {console.log(img)} */}
-          <img
-            src={require(`../../assets/images/blog/post${props.post.num}.jpg`)}
-          />
-        </div>
-        <div className="modalContent">
-          <h1>{props.post.title}</h1>
-          <p>{props.post.content}</p>
-        </div>
+        {props.children}
         <div className="closeButtonDiv">
           <img
             src={closeIcon}
