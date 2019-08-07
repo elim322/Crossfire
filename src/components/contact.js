e076a531f2b6e58d683fa4ccf2d41d177eb8c341
elim322 <einer.lim322@gmail.com>
2019-08-07 14:46:01 -0700

    style contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,9 @@ import React from "react"
 
 const Contact = () => (
   <>
-    <h1>Contact</h1>
+    <div className="contact-title">
+      <h1 className="contact-title-box">Sponsors and Affiliates</h1>
+    </div>
     <div className="sponsors">
       <img src={require("../assets/images/sponsors.jpg")} />
     </div>
@@ -11,8 +13,7 @@ const Contact = () => (
         <div className="map">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2520.1421972123144!2d6.805311315745134!3d50.828529979529335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bf3d9c390ec3c3%3A0xb3d598837b62d663!2sMittelstra%C3%9Fe+11%2C+50374+Erftstadt%2C+Germany!5e0!3m2!1sen!2sca!4v1565160835642!5m2!1sen!2sca"
-            width="600"
-            height="450"
+            className="iframe-map"
           />
         </div>
         <div className="contact-details">
@@ -35,15 +36,25 @@ const Contact = () => (
           <input type="hidden" name="form-name" value="contact" />
           <div className="field half first">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
+            <input type="text" name="name" id="name" className="name-input" />
           </div>
           <div className="field half">
             <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
+            <input
+              type="text"
+              name="email"
+              id="email"
+              className="email-input"
+            />
           </div>
           <div className="field">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="6" />
+            <textarea
+              name="message"
+              id="message"
+              rows="6"
+              className="message-input"
+            />
           </div>
           <ul className="actions">
             <li>
